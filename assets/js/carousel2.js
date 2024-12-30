4b772583d0d354aa746867bef890374f10a044d2
unknown <ayoub.jaa11@gmail.com>
2024-12-30 15:23:21 +0100

    First project page - rollback 99ac207

--- assets/js/carousel2.js
+++ assets/js/carousel2.js
@@ -1,7 +1,7 @@
 let slideIndex2 = 0; // Commence à l'index 0
 
 // Fonction pour déplacer le carrousel
-function moveSlide2(step) {
+function moveSlide(step) {
     const slides = document.querySelectorAll('.carousel2-item');
     slideIndex2 += step;
 
@@ -15,20 +15,12 @@ function moveSlide2(step) {
         slideIndex2 = slides.length - 1;
     }
 
-    console.log(`Moving to slide: ${slideIndex2}`);
-
     // Mettre à jour la position du carrousel
-    const carousel = document.querySelector('.carousel2');
-    carousel.style.transform = `translateX(-${slideIndex2 * 100}%)`;
-    carousel.style.transition = 'transform 0.5s ease-in-out';  // Ajout de la transition si ce n'était pas présent
+    document.querySelector('.carousel2').style.transform = `translateX(-${slideIndex2 * 100}%)`;
 }
 
 // Fonction pour naviguer directement à un persona spécifique (via la vignette)
-function moveToSlide2(index) {
-    slideIndex2 = index;
-    console.log(`Jumping to slide: ${slideIndex2}`);
-    
-    const carousel = document.querySelector('.carousel2');
-    carousel.style.transform = `translateX(-${slideIndex2 * 100}%)`;
-    carousel.style.transition = 'transform 0.5s ease-in-out';  // Ajout de la transition si ce n'était pas présent
+function moveToSlide(index2) {
+    slideIndex2 = index2;
+    document.querySelector('.carousel2').style.transform = `translateX(-${slideIndex2 * 100}%)`;
 }
