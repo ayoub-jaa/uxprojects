03c66812c9080412694893168724d36e380851d9
unknown <ayoub.jaa11@gmail.com>
2024-12-30 14:53:58 +0100

    First project page - rollback 99ac207

--- assets/js/carousel2.js
+++ assets/js/carousel2.js
@@ -1,26 +1,26 @@
-let slideIndex = 0; // Commence à l'index 0
+let slideIndex2 = 0; // Commence à l'index 0
 
 // Fonction pour déplacer le carrousel
-function moveSlide(step) {
+function moveSlide2(step) {
     const slides = document.querySelectorAll('.carousel2-item');
-    slideIndex += step;
+    slideIndex2 += step;
 
     // Si on est à la fin, revenir au début
-    if (slideIndex >= slides.length) {
-        slideIndex = 0;
+    if (slideIndex2 >= slides.length) {
+        slideIndex2 = 0;
     }
 
     // Si on est au début, aller à la fin
-    if (slideIndex < 0) {
-        slideIndex = slides.length - 1;
+    if (slideIndex2 < 0) {
+        slideIndex2 = slides.length - 1;
     }
 
     // Mettre à jour la position du carrousel
-    document.querySelector('.carousel2').style.transform = `translateX(-${slideIndex * 100}%)`;
+    document.querySelector('.carousel2').style.transform = `translateX(-${slideIndex2 * 100}%)`;
 }
 
 // Fonction pour naviguer directement à un persona spécifique (via la vignette)
-function moveToSlide(index) {
-    slideIndex = index;
-    document.querySelector('.carousel2').style.transform = `translateX(-${slideIndex * 100}%)`;
+function moveToSlide2(index) {
+    slideIndex2 = index;
+    document.querySelector('.carousel2').style.transform = `translateX(-${slideIndex2 * 100}%)`;
 }
