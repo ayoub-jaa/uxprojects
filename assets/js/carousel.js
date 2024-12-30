8a8d224443e5036ea13afed958c0796186b58f94
unknown <ayoub.jaa11@gmail.com>
2024-12-30 13:13:50 +0100

    First project page - ajout des maquettes

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,54 +1,26 @@
-// Carrousel principal (Premier)
-let slideIndexMain = 0; // Indice pour le premier carrousel
+let slideIndex = 0; // Commence à l'index 0
 
-function moveSlideMain(step) {
+// Fonction pour déplacer le carrousel
+function moveSlide(step) {
     const slides = document.querySelectorAll('.carousel-item');
-    slideIndexMain += step;
+    slideIndex += step;
 
     // Si on est à la fin, revenir au début
-    if (slideIndexMain >= slides.length) {
-        slideIndexMain = 0;
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
     }
 
     // Si on est au début, aller à la fin
-    if (slideIndexMain < 0) {
-        slideIndexMain = slides.length - 1;
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
     }
 
     // Mettre à jour la position du carrousel
-    document.querySelector('.carousel').style.transform = `translateX(-${slideIndexMain * 100}%)`;
+    document.querySelector('.carousel').style.transform = translateX(-${slideIndex * 100}%);
 }
 
-// Fonction pour naviguer directement à un élément du premier carrousel via la vignette
-function moveToSlideMain(index) {
-    slideIndexMain = index;
-    document.querySelector('.carousel').style.transform = `translateX(-${slideIndexMain * 100}%)`;
-}
-
-
-// Carrousel secondaire (Deuxième)
-let slideIndexSecondary = 0; // Indice pour le deuxième carrousel
-
-function moveSlideSecondary(step) {
-    const slides = document.querySelectorAll('.new-carousel-item');
-    slideIndexSecondary += step;
-
-    // Si on est à la fin, revenir au début
-    if (slideIndexSecondary >= slides.length) {
-        slideIndexSecondary = 0;
-    }
-
-    // Si on est au début, aller à la fin
-    if (slideIndexSecondary < 0) {
-        slideIndexSecondary = slides.length - 1;
-    }
-
-    // Mettre à jour la position du carrousel secondaire
-    document.querySelector('.new-carousel').style.transform = `translateX(-${slideIndexSecondary * 100}%)`;
-}
-
-// Fonction pour naviguer directement à un élément du deuxième carrousel via la vignette
-function moveToSlideSecondaryDirect(index) {
-    slideIndexSecondary = index;
-    document.querySelector('.new-carousel').style.transform = `translateX(-${slideIndexSecondary * 100}%)`;
+// Fonction pour naviguer directement à un persona spécifique (via la vignette)
+function moveToSlide(index) {
+    slideIndex = index;
+    document.querySelector('.carousel').style.transform = translateX(-${slideIndex * 100}%);
 }
