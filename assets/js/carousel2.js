7465220c5945a759b180e43797ba9b202b1930a6
unknown <ayoub.jaa11@gmail.com>
2024-12-30 14:30:51 +0100

    First project page - rollback 99ac207

---
+++ assets/js/carousel2.js
@@ -0,0 +1,26 @@
+let slideIndex = 0; // Commence à l'index 0
+
+// Fonction pour déplacer le carrousel
+function moveSlide(step) {
+    const slides = document.querySelectorAll('.carousel-item2');
+    slideIndex += step;
+
+    // Si on est à la fin, revenir au début
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+
+    // Si on est au début, aller à la fin
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    // Mettre à jour la position du carrousel
+    document.querySelector('.carousel2').style.transform = `translateX(-${slideIndex * 100}%)`;
+}
+
+// Fonction pour naviguer directement à un persona spécifique (via la vignette)
+function moveToSlide(index) {
+    slideIndex = index;
+    document.querySelector('.carousel2').style.transform = `translateX(-${slideIndex * 100}%)`;
+}
