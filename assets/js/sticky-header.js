238da513bc064d9d9810e163d3f99eff69a7c04f
unknown <ayoub.jaa11@gmail.com>
2024-12-31 16:52:54 +0100

    Page projet gestion du mobile - conflit des sticky - test d'un nouveau fonctionnement de double sticky vs une sticky pour les autres pages

--- assets/js/sticky-header.js
+++ assets/js/sticky-header.js
@@ -4,14 +4,31 @@ const burgerButton = document.getElementById('burger-button');
 const menuOverlay = document.getElementById('menu-overlay');
 let lastScrollY = window.scrollY;
 
+// Vérifier si on est sur une page projet
+const isProjectPage = document.body.classList.contains('project-page');
+let fixedNav = null;
+
+// Initialiser les variables spécifiques aux pages projets
+if (isProjectPage) {
+    fixedNav = document.querySelector('.fixed-nav');
+}
+
 // Gérer l'affichage du sticky header
 window.addEventListener('scroll', () => {
     if (window.scrollY < lastScrollY && window.scrollY > 100) {
         stickyHeader.classList.remove('hidden');
         stickyHeader.classList.add('visible');
+        
+        if (isProjectPage && fixedNav) {
+            fixedNav.style.top = '60px'; // Ajuster en fonction de la hauteur de stickyHeader
+        }
     } else {
         stickyHeader.classList.remove('visible');
         stickyHeader.classList.add('hidden');
+        
+        if (isProjectPage && fixedNav) {
+            fixedNav.style.top = '0'; // Replace fixedNav à son emplacement d'origine
+        }
     }
     lastScrollY = window.scrollY;
 });
@@ -19,5 +36,6 @@ window.addEventListener('scroll', () => {
 // Gérer le menu burger
 burgerButton.addEventListener('click', () => {
     const isActive = menuOverlay.classList.toggle('visible');
-    burgerButton.innerHTML = isActive ? '✕' : '&#9776;';  // Change the icon to 'X' when the menu is active
+    burgerButton.innerHTML = isActive ? '✕' : '&#9776;'; // Change l'icône en 'X' si le menu est actif
 });
+
