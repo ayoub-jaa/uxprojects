8c7317bcd95294a561f3bac0c8584a0fde51ae5d
ayoub-jaa <ayoub.jaa11@gmail.com>
2025-02-06 10:01:05 +0100

    last correction on button + 3 sec inactivity sticky

--- assets/js/sticky-header.js
+++ assets/js/sticky-header.js
@@ -4,7 +4,7 @@ const burgerButton = document.getElementById('burger-button');
 const menuOverlay = document.getElementById('menu-overlay');
 let lastScrollY = window.scrollY;
 let inactivityTimeout = null; // Pour gérer l'inactivité
-const INACTIVITY_DELAY = 5000; // 5 secondes
+const INACTIVITY_DELAY = 3000; // 3 secondes
 
 // Vérifier si on est sur une page projet
 const isProjectPage = document.body.classList.contains('project-page');
